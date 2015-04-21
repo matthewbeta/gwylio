4136e9adaabebddb745670ba794d539dc38b2521
matthewbeta <hello@matthew-jackson.com>
2015-04-21 23:42:56 +0100

    Adds better console feedback
    adds chalk
    adds unicode emoji
    updates messages
    removes unnecessary console logs

--- gwylio.js
+++ gwylio.js
@@ -3,36 +3,40 @@ var fs        = require('fs-extra');
 var gaze      = require('gaze');
 var path      = require('path');
 var log       = console.log.bind(console);
+var chalk     = require('chalk');
+
 
 gwylio.copy = function(filepath, dirSrc, dirDest) {
-      console.log(filepath, dirSrc, dirDest)
       var diff = filepath.substring(path.resolve(dirSrc).length),
           resolvedFilepath = path.resolve(dirDest + diff);
           dest = path.dirname(resolvedFilepath);
 
-      log('filepath: ' + filepath);
-      log('resolved filepath: ' + resolvedFilepath);
+      log(chalk.blue('⊂(ↁ_ↁ)つ copying', filepath, filepath, ' to ', resolvedFilepath, '...'));
 
       fs.copy(filepath, resolvedFilepath, function(err) {
         if (err) return console.error(err)
-        log("success!")
+        log(chalk.green.bold('✓ Success '));
+
       });
 }
 
 gwylio.init = function(dirSrc, dirDest) {
-  debugger
-
+  log(chalk.blue('⊂(ↁ_ↁ)つ Gwylio is starting... '))
+  log(chalk.blue('Copying all files... '))
   // copy everything
   fs.copy(dirSrc, dirDest, function(err) {
-    if (err) return console.error(err)
-    log("copied everything!")
+    if (err) return console.error(chalk.red(err))
+    log(chalk.green.bold("✓ Done!"));
   });
 }
 
 gwylio.watch = function(srcFiles, dirSrc, dirDest) {
   gaze(srcFiles, function() {
-    console.log('Now watching: ', this.relative())
+    var relFiles = JSON.stringify(this.relative());
+    log(chalk.blue('⊂(ↁ_ↁ)つ Gwylio is watching: '));
+    log(chalk.grey(relFiles));
     this.on('changed', function(filepath) {
+      log(chalk.blue('⊂(ↁoↁ)つ ', filepath, ' changed!'));
       gwylio.copy(filepath, dirSrc, dirDest);
     });
   });
